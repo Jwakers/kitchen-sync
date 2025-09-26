fa0b511ed5747b61780c5a8c16201a0e97c24189
Jack Wakeham <jackwakeham82@gmail.com>
2025-09-26 20:12:50 +0100

    feat: add recipes shema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,10 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
+import { PREPARATION_OPTIONS, RECIPE_CATEGORIES, UNITS } from "./lib/constants";
+
+const categoriesUnion = v.union(...RECIPE_CATEGORIES.map(v.literal));
+const preparationUnion = v.union(...PREPARATION_OPTIONS.map(v.literal));
+const unitsUnion = v.union(...UNITS.map(v.literal));
 
 export default defineSchema({
   users: defineTable({
@@ -7,4 +12,36 @@ export default defineSchema({
     // this the Clerk ID, stored in the subject JWT field
     externalId: v.string(),
   }).index("byExternalId", ["externalId"]),
+
+  recipes: defineTable({
+    userId: v.id("users"),
+    title: v.string(),
+    description: v.optional(v.string()),
+    prepTime: v.number(),
+    cookTime: v.number(),
+    serves: v.number(),
+    category: categoriesUnion,
+    ingredients: v.array(
+      v.object({
+        ingredientId: v.id("ingredients"),
+        amount: v.number(),
+        unit: v.optional(unitsUnion),
+        preparation: v.optional(preparationUnion),
+      })
+    ),
+    method: v.array(
+      v.object({
+        step: v.string(),
+        image: v.optional(v.id("_storage")),
+      })
+    ),
+    updatedAt: v.number(),
+  })
+    .index("by_user", ["userId"])
+    .index("by_category", ["category"])
+    .index("by_user_and_category", ["userId", "category"]),
+
+  ingredients: defineTable({
+    name: v.string(),
+  }),
 });
