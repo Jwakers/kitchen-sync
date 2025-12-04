b3832569149baa6bb19e35f3c2b74776b3b101b1
Jack Wakeham <jackwakeham82@gmail.com>
2025-12-04 10:23:51 +0000

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -244,8 +244,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.png", sizes: "180x180", type: "image/png" },
       { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
     ],
     apple: "/apple-touch-icon.png",
